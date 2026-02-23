ac554eaa49e9bff28b1625a1e8de3e642c09bd31
ketsutt-coder <sfmf8fhr8m@privaterelay.appleid.com>
2026-02-22 22:23:01 -0600

    Update HighTexturesPopup.cpp

--- src/classes/HighTexturesPopup.cpp
+++ src/classes/HighTexturesPopup.cpp
@@ -4,24 +4,21 @@
 
 using namespace geode::utils::file;
 
-CCMenuItemSpriteExtra* HighTexturesPopup::createButton(const char* text, float width, const char* sprite, std::string id, SEL_MenuHandler selector) {
-    CCSize size = m_mainLayer->getContentSize();
-    auto spr = ButtonSprite::create(text, width, true, "bigFont.fnt", sprite, 30, 0.65f);
-    auto btn = CCMenuItemSpriteExtra::create(spr, this, selector);
-    btn->setVisible(false);
-    btn->setPosition({ size.width/2, 25 });
-    btn->setContentSize({ width, 30 });
-    spr->setPosition(btn->getContentSize()/2);
-    btn->setID(id);
-    m_menu->addChild(btn);
-    return btn;
+HighTexturesPopup* HighTexturesPopup::create(bool zipExists) {
+    auto ret = new HighTexturesPopup();
+    if (ret && ret->initAnchored(360.f, 200.f, zipExists)) {
+        ret->autorelease();
+        return ret;
+    }
+    CC_SAFE_DELETE(ret);
+    return nullptr;
 }
 
 void HighTexturesPopup::keyDown(cocos2d::enumKeyCodes key, double p1) {
     if (key == cocos2d::enumKeyCodes::KEY_Escape) return;
     if (m_finished && key == cocos2d::enumKeyCodes::KEY_Space) {
         Mod::get()->setSavedValue("applied", false);
-        game::restart(true); // Corregido: pide argumento
+        game::restart(true); // Argumento obligatorio en v5
         return;
     }
     Popup::keyDown(key, p1);
@@ -29,67 +26,26 @@ void HighTexturesPopup::keyDown(cocos2d::enumKeyCodes key, double p1) {
 
 bool HighTexturesPopup::setup(bool zipExists) {
     this->setTitle("High Textures");
-    CCSize size = m_mainLayer->getContentSize();
+    auto size = m_mainLayer->getContentSize();
     m_gameVersion = Loader::get()->getGameVersion();
+
+    m_chatLabel = CCLabelBMFont::create("Textures missing. Download now?", "chatFont.fnt");
+    m_chatLabel->setPosition({ size.width/2, size.height - 70 });
+    m_mainLayer->addChild(m_chatLabel);
+
+    m_menu = CCMenu::create();
+    m_menu->setPosition({0, 0});
+    m_mainLayer->addChild(m_menu);
+
+    m_downloadBtn = createButton("Download", 100, "GJ_button_01.png", "download-btn", menu_selector(HighTexturesPopup::onDownload));
+    m_restartBtn = createButton("Restart", 100, "GJ_button_02.png", "restart-btn", menu_selector(HighTexturesPopup::onRestart));
+    m_extractBtn = createButton("Extract", 100, "GJ_button_01.png", "extract-btn", menu_selector(HighTexturesPopup::onExtract));
+    m_retryBtn = createButton("Retry", 100, "GJ_button_04.png", "retry-btn", menu_selector(HighTexturesPopup::onRetry));
+
+    m_downloadBtn->setVisible(!zipExists);
+    m_extractBtn->setVisible(zipExists);
     
-    auto chatLabel = CCLabelBMFont::create("Necessary high graphics textures missing. Download now? (107MB)", "chatFont.fnt");
-    chatLabel->setPosition({ size.width/2, size.height - 70 });
-    chatLabel->setScale(0.9f);
-    chatLabel->setID("chat-label");
-    m_mainLayer->addChild(chatLabel);
-    m_chatLabel = chatLabel;
-
-    auto menu = CCMenu::create();
-    menu->setPosition({ 0, 0 });
-    menu->setID("buttons-menu");
-    m_mainLayer->addChild(menu);
-    m_menu = menu;
-
-    m_downloadBtn = createButton("Download", 104, "GJ_button_01.png", "download-button", menu_selector(HighTexturesPopup::onDownload));
-    m_extractBtn = createButton("Extract", 104, "GJ_button_02.png", "extract-button", menu_selector(HighTexturesPopup::onExtract));
-    m_retryBtn = createButton("Retry", 104, "GJ_button_04.png", "retry-button", menu_selector(HighTexturesPopup::onRetry));
-    m_restartBtn = createButton("Restart", 104, "GJ_button_01.png", "restart-button", menu_selector(HighTexturesPopup::onRestart));
-    m_hideBtn = createButton("Run in Background", 180, "GJ_button_03.png", "hide-button", menu_selector(HighTexturesPopup::onHide));
-
-    auto progressBG = CCSprite::create("GJ_progressBar_001.png");
-    progressBG->setVisible(false);
-    progressBG->setPosition({ size.width/2, 60 });
-    progressBG->setID("progress-bar-background");
-    m_mainLayer->addChild(progressBG);
-    m_progressBG = progressBG;
-
-    auto downloadStencil = CCScale9Sprite::create("square02_001.png");
-    downloadStencil->setAnchorPoint({ 0, 0.5f });
-    m_downloadStencil = downloadStencil;
-
-    auto extractStencil = CCScale9Sprite::create("square02_001.png");
-    extractStencil->setAnchorPoint({ 0, 0.5f });
-    m_extractStencil = extractStencil;
-
-    m_downloadLabel = CCLabelBMFont::create("0.00%", "bigFont.fnt");
-    m_downloadLabel->setVisible(false);
-    m_mainLayer->addChild(m_downloadLabel);
-
-    m_extractLabel = CCLabelBMFont::create("0.00%", "bigFont.fnt");
-    m_extractLabel->setVisible(false);
-    m_mainLayer->addChild(m_extractLabel);
-
-    if (zipExists) {
-        m_extractBtn->setVisible(true);
-    } else {
-        m_downloadBtn->setVisible(true);
-    }
     return true;
-}
-
-HighTexturesPopup* HighTexturesPopup::create(bool zipExists) {
-    auto ret = new HighTexturesPopup();
-    if (ret && ret->initAnchored(360.f, 200.f, zipExists)) {
-        ret->autorelease();
-        return ret;
-    }
-    delete ret;
-    return nullptr;
 }
 
 void HighTexturesPopup::onDownload(CCObject* sender) { startDownload(); }
@@ -99,35 +55,16 @@ void HighTexturesPopup::onExtract(CCObject* sender) {
 }
 void HighTexturesPopup::onRetry(CCObject* sender) { onDownload(nullptr); }
 void HighTexturesPopup::onRestart(CCObject* sender) { game::restart(true); }
-void HighTexturesPopup::onHide(CCObject* sender) { this->setVisible(false); }
-
-void HighTexturesPopup::setDownloadPercentage(float percentage, ccColor3B color) {
-    m_downloadPercentage = percentage;
-    m_downloadStencil->setScaleX(m_downloadPercentage / 100);
-}
-
-void HighTexturesPopup::setExtractPercentage(float percentage, ccColor3B color) {
-    m_extractPercentage = percentage;
-    m_extractStencil->setScaleX(m_extractPercentage / 100);
-}
 
 void HighTexturesPopup::startDownload() {
-    m_progressBG->setVisible(true);
-    m_downloadLabel->setVisible(true);
     fs::path path = Mod::get()->getConfigDir();
-
     m_downloadListener.bind([=, this](web::WebTask::Event* e) {
         if (auto res = e->getValue()) {
             if (res->ok()) {
                 downloadSucceeded(path / (m_gameVersion + ".zip"), path);
-            } else {
-                downloadFailed("Server error");
             }
-        } else if (auto p = e->getProgress()) {
-            setDownloadPercentage(p->downloadProgress().value_or(0.f), {255, 255, 255});
         }
     });
-
     auto req = web::WebRequest();
     m_downloadListener.setFilter(req.get(m_links[m_gameVersion][0]));
 }
@@ -135,16 +72,22 @@ void HighTexturesPopup::startDownload() {
 void HighTexturesPopup::startExtract(fs::path file, fs::path path) {
     std::thread([=, this]() {
         auto res = file::Unzip::intoDir(file, path, true);
-        Loader::get()->queueInMainThread([=, this]() {
-            if (res.isOk()) extractSucceeded();
-            else extractFailed("Zip error");
+        Loader::get()->queueInMainThread([=, this, res]() {
+            if (res.isOk()) {
+                m_finished = true;
+                m_restartBtn->setVisible(true);
+            }
         });
     }).detach();
 }
 
 void HighTexturesPopup::downloadSucceeded(fs::path file, fs::path path) { startExtract(file, path); }
 void HighTexturesPopup::downloadFailed(std::string reason) { log::error("{}", reason); }
-void HighTexturesPopup::extractSucceeded() { m_finished = true; m_restartBtn->setVisible(true); }
-void HighTexturesPopup::extractFailed(std::string reason) { log::error("{}", reason); }
-void HighTexturesPopup::notifySuccess() {}
-void HighTexturesPopup::notifyFailure(std::string which, std::string reason) {}
+
+CCMenuItemSpriteExtra* HighTexturesPopup::createButton(const char* text, float width, const char* sprite, std::string id, SEL_MenuHandler selector) {
+    auto spr = ButtonSprite::create(text, width, true, "bigFont.fnt", sprite, 30, 0.6f);
+    auto btn = CCMenuItemSpriteExtra::create(spr, this, selector);
+    btn->setPosition({ m_mainLayer->getContentSize().width / 2, 30 });
+    m_menu->addChild(btn);
+    return btn;
+}
