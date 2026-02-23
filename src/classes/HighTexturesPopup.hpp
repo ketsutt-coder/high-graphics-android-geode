f31b187d2b1f718cf1ac5d00816de2af74e21229
ketsutt-coder <sfmf8fhr8m@privaterelay.appleid.com>
2026-02-22 22:15:55 -0600

    Update HighTexturesPopup.hpp

--- src/classes/HighTexturesPopup.hpp
+++ src/classes/HighTexturesPopup.hpp
@@ -1,5 +1,4 @@
 #pragma once
-
 #include <Geode/Geode.hpp>
 #include <Geode/utils/web.hpp>
 
@@ -8,60 +7,34 @@ namespace fs = std::filesystem;
 
 class HighTexturesPopup : public geode::Popup<bool> {
 protected:
+    // Firma correcta para setup en Geode v5
     bool setup(bool zipExists) override;
 
 public:
     inline static std::map<std::string, std::vector<std::string>> m_links = {
-        {"2.2074", {"https://github.com/Weebifying/gd-textures/releases/download/2.2074/2.2074.zip", "https://files.catbox.moe/buykym.zip", "https://archive.org/download/2.2074/2.2074.zip"}},
-        {"2.2081", {"https://github.com/Weebifying/gd-textures/releases/download/2.2074/2.2074.zip", "https://files.catbox.moe/buykym.zip", "https://archive.org/download/2.2074/2.2074.zip"}}
+        {"2.2081", {"https://github.com/Weebifying/gd-textures/releases/download/2.2074/2.2074.zip", "https://files.catbox.moe/buykym.zip"}}
     };
 
     float m_downloadPercentage = 0.f;
-    float m_extractPercentage = 0.f;
     std::string m_gameVersion;
-    bool m_zipExists;
-    bool m_isHidden;
     bool m_finished = false;
 
-    CCMenu* m_menu;
-    CCSprite* m_progressBG;
-    CCLabelBMFont* m_chatLabel;
-    CCMenuItemSpriteExtra* m_retryBtn;
-    CCMenuItemSpriteExtra* m_restartBtn;
-    CCMenuItemSpriteExtra* m_hideBtn;
+    CCLabelBMFont* m_chatLabel = nullptr;
+    CCMenuItemSpriteExtra* m_restartBtn = nullptr;
+    CCMenuItemSpriteExtra* m_downloadBtn = nullptr;
+    CCSprite* m_progressBG = nullptr;
+    CCNode* m_downloadStencil = nullptr;
 
-    CCNode* m_downloadStencil;
-    CCNode* m_extractStencil;
-    CCLabelBMFont* m_downloadLabel;
-    CCLabelBMFont* m_extractLabel;
-    CCMenuItemSpriteExtra* m_downloadBtn;
-    CCMenuItemSpriteExtra* m_extractBtn;
-
-    // Cambiado a WebResponseTask para Geode v5
+    // WebResponseTask es el estándar actual en Geode v5
     EventListener<web::WebResponseTask> m_downloadListener;
 
     static HighTexturesPopup* create(bool zipExists);
-    CCMenuItemSpriteExtra* createButton(const char* text, float width, const char* sprite, std::string id, SEL_MenuHandler selector);
     
-    // Corregido: Ahora recibe el double p1 que pide Geode v5
+    // Firma de keyDown actualizada para evitar el error de "non-virtual"
     void keyDown(cocos2d::enumKeyCodes key, double p1) override;
 
     void onDownload(CCObject* sender);
-    void onExtract(CCObject* sender);
-    void onRetry(CCObject* sender);
     void onRestart(CCObject* sender);
-    void onHide(CCObject* sender);
-
-    void setDownloadPercentage(float percentage, ccColor3B color);
-    void setExtractPercentage(float percentage, ccColor3B color);
-
     void startDownload();
     void startExtract(fs::path file, fs::path path);
-
-    void downloadSucceeded(fs::path file, fs::path path);
-    void downloadFailed(std::string reason);
-    void extractSucceeded();
-    void extractFailed(std::string reason);
-    void notifySuccess();
-    void notifyFailure(std::string which, std::string reason);
 };
