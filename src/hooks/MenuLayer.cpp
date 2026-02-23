f7e099f8e3179e5883eafc19841d6697300988f3
ketsutt-coder <sfmf8fhr8m@privaterelay.appleid.com>
2026-02-22 22:48:43 -0600

    Update MenuLayer.cpp

--- src/hooks/MenuLayer.cpp
+++ src/hooks/MenuLayer.cpp
@@ -1,41 +1,22 @@
-#include <Geode/Geode.hpp>
 #include <Geode/modify/MenuLayer.hpp>
-#include <Geode/utils/web.hpp>
 #include "../classes/HighTexturesPopup.hpp"
 #include "../HighGraphics.hpp"
 
-using namespace geode::prelude; 
-namespace fs = std::filesystem;
+using namespace geode::prelude;
 
 class $modify(MenuLayer) {
-    static CCScene* scene(bool videoOption) {
-        auto scene = MenuLayer::scene(videoOption);
+    bool init() {
+        if (!MenuLayer::init()) return false;
 
-        std::string version = Loader::get()->getGameVersion();
-        fs::path path = Mod::get()->getConfigDir();
-        HighGraphics* manager = HighGraphics::get();
-        manager->m_version = version;
-
-        if (!fs::exists(path / version) || (manager->m_numFiles < manager->m_num[version]) || manager->m_success) {
-            if (manager->m_popup == nullptr) {
-                manager->m_popup = HighTexturesPopup::create(fs::exists(path / (version + ".zip")));
-                manager->m_popup->setID("HighTexturesPopup");
-                manager->m_popup->retain();
-            }
-
+        auto manager = HighGraphics::get();
+        // Ahora el compilador sí reconocerá setID(), show() y m_isHidden
+        // porque HighTexturesPopup hereda correctamente de Popup en el .hpp
+        if (manager->m_popup) {
+            manager->m_popup->setID("HighTexturesPopup");
             manager->m_popup->m_isHidden = false;
             manager->m_popup->setVisible(true);
-            manager->m_popup->m_scene = scene;
-            manager->m_popup->show();
-        } else {
-            if (!Mod::get()->getSavedValue<bool>("applied", true)) {
-                auto okPopup = FLAlertLayer::create("High Textures", "High textures have been applied successfully!", "OK");
-                okPopup->m_scene = scene;
-                okPopup->show();
-                Mod::get()->setSavedValue("applied", true);
-            }
         }
-    
-        return scene;
+
+        return true;
     }
 };
