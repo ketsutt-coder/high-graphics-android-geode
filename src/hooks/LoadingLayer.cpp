074e1404230d6b9eb6e29a1718c9fef29eda5553
ketsutt-coder <sfmf8fhr8m@privaterelay.appleid.com>
2026-02-22 22:05:30 -0600

    Update LoadingLayer.cpp

--- src/hooks/LoadingLayer.cpp
+++ src/hooks/LoadingLayer.cpp
@@ -1,20 +1,2 @@
-#include <Geode/Geode.hpp>
-#include <Geode/modify/LoadingLayer.hpp>
-#include "../HighGraphics.hpp"
-using namespace geode::prelude; 
-namespace fs = std::filesystem;
-
-class $modify(LoadingLayer) {
-    // load saved graphics setting instead of medium graphics on startup
-    bool init(bool p0) {
-        std::string version = Loader::get()->getGameVersion();
-        fs::path path = Mod::get()->getConfigDir();
-        HighGraphics* manager = HighGraphics::get();
-
-        if (fs::exists(path / version) && (manager->m_numFiles >= manager->m_num[version])) {
-            CCDirector::get()->updateContentScale(as<TextureQuality>(GameManager::sharedState()->m_texQuality));
-        }
-        
-        return LoadingLayer::init(p0);
-    }
-};
+// Busca la parte de CCDirector y ponla así:
+CCDirector::get()->updateContentScale(static_cast<TextureQuality>(GameManager::sharedState()->m_texQuality));
