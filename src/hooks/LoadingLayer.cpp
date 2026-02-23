c611d88ff5d7d185fe470e6132a462c601e2922b
ketsutt-coder <sfmf8fhr8m@privaterelay.appleid.com>
2026-02-22 22:16:17 -0600

    Update LoadingLayer.cpp

--- src/hooks/LoadingLayer.cpp
+++ src/hooks/LoadingLayer.cpp
@@ -1,2 +1,16 @@
-// Busca la parte de CCDirector y ponla así:
-CCDirector::get()->updateContentScale(static_cast<TextureQuality>(GameManager::sharedState()->m_texQuality));
+#include <Geode/modify/LoadingLayer.hpp>
+#include "../HighGraphics.hpp"
+
+using namespace geode::prelude;
+
+class $modify(LoadingLayer) {
+    void loadAssets() {
+        LoadingLayer::loadAssets();
+        
+        // En Geode v5, usamos static_cast para tipos definidos
+        auto texQuality = GameManager::sharedState()->m_texQuality;
+        auto quality = static_cast<TextureQuality>(static_cast<int>(texQuality));
+        
+        CCDirector::get()->updateContentScale(quality);
+    }
+};
