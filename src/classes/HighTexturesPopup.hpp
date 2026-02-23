37d30cf911b2a201cba7c8b00fb1092627f935b2
ketsutt-coder <sfmf8fhr8m@privaterelay.appleid.com>
2026-02-22 22:22:37 -0600

    Update HighTexturesPopup.hpp

--- src/classes/HighTexturesPopup.hpp
+++ src/classes/HighTexturesPopup.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <Geode/Geode.hpp>
+#include <Geode/ui/Popup.hpp> // Obligatorio en Geode v5
 #include <Geode/utils/web.hpp>
 
 using namespace geode::prelude;
@@ -7,34 +8,42 @@ namespace fs = std::filesystem;
 
 class HighTexturesPopup : public geode::Popup<bool> {
 protected:
-    // Firma correcta para setup en Geode v5
     bool setup(bool zipExists) override;
 
 public:
-    inline static std::map<std::string, std::vector<std::string>> m_links = {
-        {"2.2081", {"https://github.com/Weebifying/gd-textures/releases/download/2.2074/2.2074.zip", "https://files.catbox.moe/buykym.zip"}}
-    };
+    static HighTexturesPopup* create(bool zipExists);
 
+    // Declaraciones faltantes que causaban errores de "out-of-line definition"
+    CCMenuItemSpriteExtra* createButton(const char* text, float width, const char* sprite, std::string id, SEL_MenuHandler selector);
+    void keyDown(cocos2d::enumKeyCodes key, double p1) override;
+    
+    void onDownload(CCObject* sender);
+    void onExtract(CCObject* sender);
+    void onRetry(CCObject* sender);
+    void onRestart(CCObject* sender);
+    
+    void startDownload();
+    void startExtract(fs::path file, fs::path path);
+    void downloadSucceeded(fs::path file, fs::path path);
+    void downloadFailed(std::string reason);
+
+    // Miembros de clase para control de UI
     float m_downloadPercentage = 0.f;
     std::string m_gameVersion;
     bool m_finished = false;
-
     CCLabelBMFont* m_chatLabel = nullptr;
     CCMenuItemSpriteExtra* m_restartBtn = nullptr;
     CCMenuItemSpriteExtra* m_downloadBtn = nullptr;
+    CCMenuItemSpriteExtra* m_extractBtn = nullptr;
+    CCMenuItemSpriteExtra* m_retryBtn = nullptr;
+    CCMenu* m_menu = nullptr;
     CCSprite* m_progressBG = nullptr;
     CCNode* m_downloadStencil = nullptr;
 
-    // WebResponseTask es el estándar actual en Geode v5
-    EventListener<web::WebResponseTask> m_downloadListener;
-
-    static HighTexturesPopup* create(bool zipExists);
+    // WebTask es el nombre correcto en la v5
+    EventListener<web::WebTask> m_downloadListener;
     
-    // Firma de keyDown actualizada para evitar el error de "non-virtual"
-    void keyDown(cocos2d::enumKeyCodes key, double p1) override;
-
-    void onDownload(CCObject* sender);
-    void onRestart(CCObject* sender);
-    void startDownload();
-    void startExtract(fs::path file, fs::path path);
+    inline static std::map<std::string, std::vector<std::string>> m_links = {
+        {"2.2081", {"https://github.com/Weebifying/gd-textures/releases/download/2.2074/2.2074.zip"}}
+    };
 };
